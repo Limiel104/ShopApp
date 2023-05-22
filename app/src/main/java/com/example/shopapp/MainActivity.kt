7dc466e9a8c63c19f82183ba40042f044ef301d3
Limiel104 <a.myjak1995@gmail.com>
2023-05-22 20:45:27 +0200

    Login screen composables

--- app/src/main/java/com/example/shopapp/MainActivity.kt
+++ app/src/main/java/com/example/shopapp/MainActivity.kt
@@ -3,13 +3,23 @@ package com.example.shopapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.testTag
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.input.VisualTransformation
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.shopapp.ui.theme.ShopAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -26,6 +36,238 @@ class MainActivity : ComponentActivity() {
                 }
             }
         }
+    }
+}
+
+@Composable
+fun ShopTextFieldItem(
+    text: String,
+    label: String,
+    placeholder: String,
+    testTag: String,
+    isError: Boolean,
+    onValueChange: (String) -> Unit,
+    keyboardOptions: KeyboardOptions = KeyboardOptions.Default,
+    visualTransformation: VisualTransformation = VisualTransformation.None
+) {
+    OutlinedTextField(
+        value = text,
+        singleLine = true,
+        isError = isError,
+        label = { Text(label) },
+        placeholder = { Text(placeholder) },
+        onValueChange = { onValueChange(it) },
+        keyboardOptions = keyboardOptions,
+        visualTransformation = visualTransformation,
+        modifier = Modifier
+            .background(MaterialTheme.colors.background)
+            .fillMaxWidth()
+            .testTag(testTag)
+    )
+}
+
+@Composable
+@Preview
+fun ChatTextFieldItemPreview() {
+    ShopAppTheme {
+        ShopTextFieldItem(
+            text = "",
+            label = "Text",
+            placeholder = "Placeholder",
+            testTag = "testTag",
+            isError = false,
+            onValueChange = {}
+        )
+    }
+}
+
+@Composable
+@Preview
+fun ChatTextFieldItemWithErrorPreview() {
+    ShopAppTheme {
+        ShopTextFieldItem(
+            text = "",
+            label = "Text",
+            placeholder = "Placeholder",
+            testTag = "testTag",
+            isError = true,
+            onValueChange = {}
+        )
+    }
+}
+
+@Composable
+fun ShopButtonItem(
+    text: String,
+    testTag: String,
+    onClick: () -> Unit
+) {
+    OutlinedButton(
+        modifier = Modifier
+            .fillMaxWidth()
+            .testTag(testTag),
+        colors = ButtonDefaults.buttonColors(MaterialTheme.colors.secondary),
+        onClick = { onClick() }
+    ) {
+        Text(
+            text = text,
+            color = MaterialTheme.colors.onSecondary,
+            modifier = Modifier
+                .padding(7.dp)
+        )
+    }
+}
+
+@Preview
+@Composable
+fun ShopButtonItemPreview() {
+    ShopAppTheme {
+        ShopButtonItem(
+            text = "Text",
+            testTag = "testTag",
+            onClick = {}
+        )
+    }
+}
+
+@Composable
+fun ErrorTextFieldItem(
+    errorMessage: String,
+    testTag: String
+) {
+    Text(
+        text = errorMessage,
+        fontSize = 13.sp,
+        color = MaterialTheme.colors.error,
+        textAlign = TextAlign.Start,
+        modifier = Modifier
+            .testTag(testTag)
+    )
+}
+
+@Preview
+@Composable
+fun ErrorTextFieldItemPreview() {
+    ShopAppTheme {
+        ErrorTextFieldItem(
+            errorMessage = "Error message",
+            testTag = "tag"
+        )
+    }
+}
+
+@Composable
+fun LoginScreen() {
+
+    val email = "email@email.com"
+    val emailError = ""
+    val password = "password"
+    val passwordError = ""
+    val isLoading = false
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(MaterialTheme.colors.background)
+            .padding(horizontal = 20.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.SpaceEvenly
+    ) {
+        Text(
+            text = "Hello",
+            color = MaterialTheme.colors.secondary,
+            fontSize = 32.sp,
+            fontWeight = FontWeight.SemiBold
+        )
+
+        Column {
+            ShopTextFieldItem(
+                text = email,
+                label = "email",
+                placeholder = "placeholder",
+                testTag = "email",
+                isError = emailError != null,
+                onValueChange = {},
+                keyboardOptions = KeyboardOptions(
+                    keyboardType = KeyboardType.Email
+                )
+            )
+
+            if(emailError != null) {
+                ErrorTextFieldItem(
+                    errorMessage = emailError,
+                    testTag = "errorTag"
+                )
+            }
+
+            Spacer(modifier = Modifier.height(10.dp))
+
+            ShopTextFieldItem(
+                text = password,
+                label = "password",
+                placeholder = "placeholder",
+                testTag = "tag",
+                isError = passwordError != null,
+                onValueChange = {},
+                keyboardOptions = KeyboardOptions(
+                    keyboardType = KeyboardType.Password
+                ),
+                visualTransformation = PasswordVisualTransformation()
+            )
+
+            if(passwordError != null) {
+                ErrorTextFieldItem(
+                    errorMessage = passwordError,
+                    testTag = "tag"
+                )
+            }
+        }
+
+        ShopButtonItem(
+            text = "Login",
+            testTag = "tag",
+            onClick = {}
+        )
+
+        Row(
+            modifier = Modifier
+                .fillMaxWidth(),
+            horizontalArrangement = Arrangement.Center
+        ) {
+            Text(
+                text = "Don't have an account?"
+            )
+
+            Spacer(modifier = Modifier.width(5.dp))
+
+            Text(
+                text = "Sign up",
+                color = MaterialTheme.colors.primary,
+                fontWeight = FontWeight.SemiBold,
+                modifier = Modifier
+                    .clickable {}
+                    .testTag("tag")
+            )
+        }
+    }
+
+    if(isLoading) {
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+                .testTag("tag"),
+            contentAlignment = Alignment.Center
+        ) {
+            CircularProgressIndicator()
+        }
+    }
+}
+
+@Preview
+@Composable
+fun LoginScreenPreview() {
+    ShopAppTheme() {
+        LoginScreen()
     }
 }
 
