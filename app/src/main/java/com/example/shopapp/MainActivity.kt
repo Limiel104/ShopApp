db200c7e7dddf86525259cb12709a4d80dd19e52
Limiel104 <a.myjak1995@gmail.com>
2023-05-30 21:22:00 +0200

    Category screen and product item composables

--- app/src/main/java/com/example/shopapp/MainActivity.kt
+++ app/src/main/java/com/example/shopapp/MainActivity.kt
@@ -6,14 +6,16 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.outlined.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
@@ -41,116 +43,115 @@ class MainActivity : ComponentActivity() {
     }
 }
 
-@Composable
-@Preview
-fun ShopTextFieldItemPreview() {
-    ShopAppTheme {
-        ShopTextFieldItem(
-            text = "",
-            label = "Text",
-            placeholder = "Placeholder",
-            testTag = "testTag",
-            isError = false,
-            onValueChange = {}
-        )
-    }
-}
-
-@Composable
-@Preview
-fun ShopTextFieldItemWithErrorPreview() {
-    ShopAppTheme {
-        ShopTextFieldItem(
-            text = "",
-            label = "Text",
-            placeholder = "Placeholder",
-            testTag = "testTag",
-            isError = true,
-            onValueChange = {}
-        )
-    }
-}
-
-@Preview
-@Composable
-fun ShopButtonItemPreview() {
-    ShopAppTheme {
-        ShopButtonItem(
-            text = "Text",
-            testTag = "testTag",
-            onClick = {}
-        )
-    }
-}
-
-@Preview
-@Composable
-fun ErrorTextFieldItemPreview() {
-    ShopAppTheme {
-        ErrorTextFieldItem(
-            errorMessage = "Error message",
-            testTag = "tag"
-        )
-    }
-}
-
-@Preview
-@Composable
-fun SignupScreenPreview() {
-    ShopAppTheme() {
-        SignupScreen()
-    }
-}
-
-@Preview
-@Composable
-fun HomeScreenPreview() {
-    ShopAppTheme {
-        HomeScreen()
-    }
-}
-
-@Preview
-@Composable
-fun HomeTopBarPreview() {
-    ShopAppTheme {
-        HomeTopBar()
-    }
-}
-
-@Preview
-@Composable
-fun BottomBarPreview() {
-    ShopAppTheme {
-        BottomBar()
-    }
-}
-
-@Preview
-@Composable
-fun BottomBarItemPreview() {
-    ShopAppTheme {
-        BottomBarItem(
-            text = "Favourites",
-            icon = Icons.Outlined.FavoriteBorder,
-            onClick = {}
-        )
-    }
-}
-
-@Preview
-@Composable
-fun OfferItemPreview() {
-    ShopAppTheme {
-        OfferItem(
-            text = "All clothes for women now 10% cheaper"
-        )
-    }
-}
+//@Composable
+//@Preview
+//fun ShopTextFieldItemPreview() {
+//    ShopAppTheme {
+//        ShopTextFieldItem(
+//            text = "",
+//            label = "Text",
+//            placeholder = "Placeholder",
+//            testTag = "testTag",
+//            isError = false,
+//            onValueChange = {}
+//        )
+//    }
+//}
+//
+//@Composable
+//@Preview
+//fun ShopTextFieldItemWithErrorPreview() {
+//    ShopAppTheme {
+//        ShopTextFieldItem(
+//            text = "",
+//            label = "Text",
+//            placeholder = "Placeholder",
+//            testTag = "testTag",
+//            isError = true,
+//            onValueChange = {}
+//        )
+//    }
+//}
+//
+//@Preview
+//@Composable
+//fun ShopButtonItemPreview() {
+//    ShopAppTheme {
+//        ShopButtonItem(
+//            text = "Text",
+//            testTag = "testTag",
+//            onClick = {}
+//        )
+//    }
+//}
+//
+//@Preview
+//@Composable
+//fun ErrorTextFieldItemPreview() {
+//    ShopAppTheme {
+//        ErrorTextFieldItem(
+//            errorMessage = "Error message",
+//            testTag = "tag"
+//        )
+//    }
+//}
+//
+//@Preview
+//@Composable
+//fun SignupScreenPreview() {
+//    ShopAppTheme() {
+//        SignupScreen()
+//    }
+//}
+//
+//@Preview
+//@Composable
+//fun HomeScreenPreview() {
+//    ShopAppTheme {
+//        HomeScreen()
+//    }
+//}
+//
+//@Preview
+//@Composable
+//fun HomeTopBarPreview() {
+//    ShopAppTheme {
+//        HomeTopBar()
+//    }
+//}
+//
+//@Preview
+//@Composable
+//fun BottomBarPreview() {
+//    ShopAppTheme {
+//        BottomBar()
+//    }
+//}
+//
+//@Preview
+//@Composable
+//fun BottomBarItemPreview() {
+//    ShopAppTheme {
+//        BottomBarItem(
+//            text = "Favourites",
+//            icon = Icons.Outlined.FavoriteBorder,
+//            onClick = {}
+//        )
+//    }
+//}
+//
+//@Preview
+//@Composable
+//fun OfferItemPreview() {
+//    ShopAppTheme {
+//        OfferItem(
+//            text = "All clothes for women now 10% cheaper"
+//        )
+//    }
+//}
 
 @Composable
 fun CategoryScreen() {
-    val products = listOf(1,2,3,4)
     val scaffoldState = rememberScaffoldState()
 
     Scaffold(
@@ -167,16 +168,14 @@ fun CategoryScreen() {
     ) { innerPadding ->
         Column(
             modifier = Modifier
+                .fillMaxSize()
                 .padding(innerPadding)
         ) {
-            LazyColumn(
-                modifier = Modifier
-                    .fillMaxSize(),
+            LazyVerticalGrid(
+                columns = GridCells.Fixed(2)
             ) {
-                itemsIndexed(products) { _, product ->
-                    ProductItem(
-
-                    )
+                items(20) { i ->
+                    ProductItem()
                 }
             }
         }
@@ -242,25 +241,33 @@ fun CategoryTopBarPreview() {
 
 @Composable
 fun ProductItem() {
-    Column() {
+    Column(
+        modifier = Modifier.padding(top = 5.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
+    ) {
         Card(
             modifier = Modifier
-                .size(150.dp,200.dp)
+                .size(180.dp, 200.dp)
+                .padding(bottom = 15.dp)
+                .padding(horizontal = 20.dp),
+            backgroundColor = Color.Yellow
         ) {
             AsyncImage(
                 model = ImageRequest
                     .Builder(LocalContext.current)
-                    .data(R.drawable.ic_no_image)
+                    .data("https://fakestoreapi.com/img/71-3HjGNDUL._AC_SY879._SX._UX._SY._UY_.jpg")
                     .crossfade(true)
                     .placeholder(R.drawable.ic_no_image)
                     .build(),
                 contentDescription = "Image",
                 fallback = painterResource(R.drawable.ic_no_image),
-                error = painterResource(R.drawable.ic_no_image)
+                error = painterResource(R.drawable.ic_no_image),
+                contentScale = ContentScale.FillBounds
             )
         }
     }
 }
+
 
 @Preview
 @Composable
