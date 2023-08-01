887186e0baf1b488e3d501d7b7e9fbee2fc694c6
Limiel104 <a.myjak1995@gmail.com>
2023-08-01 10:38:08 +0200

    Added SetUserFavouritesUseCaseTest

--- app/src/main/java/com/example/shopapp/domain/use_case/SetUserFavouritesUseCase.kt
+++ app/src/main/java/com/example/shopapp/domain/use_case/SetUserFavouritesUseCase.kt
@@ -5,27 +5,33 @@ import com.example.shopapp.domain.model.Product
 
 class SetUserFavouritesUseCase {
     operator fun invoke(products: List<Product>, favourites: List<Favourite>): List<Product> {
-        val productsWithUserFavourites: MutableList<Product> = mutableListOf()
-        val indexes: MutableList<Int> = mutableListOf()
 
-        for(favourite in favourites) {
-            indexes.add(favourite.productId)
+        if(products.isEmpty() || favourites.isEmpty()){
+            return products
         }
+        else {
+            val productsWithUserFavourites: MutableList<Product> = mutableListOf()
+            val indexes: MutableList<Int> = mutableListOf()
 
-        for(product in products) {
-            productsWithUserFavourites.add(
-                Product(
-                    id = product.id,
-                    title = product.title,
-                    price = product.price,
-                    description = product.description,
-                    category = product.category,
-                    imageUrl = product.imageUrl,
-                    isInFavourites = indexes.contains(product.id)
+            for(favourite in favourites) {
+                indexes.add(favourite.productId)
+            }
+
+            for(product in products) {
+                productsWithUserFavourites.add(
+                    Product(
+                        id = product.id,
+                        title = product.title,
+                        price = product.price,
+                        description = product.description,
+                        category = product.category,
+                        imageUrl = product.imageUrl,
+                        isInFavourites = indexes.contains(product.id)
+                    )
                 )
-            )
-        }
+            }
 
-        return productsWithUserFavourites
+            return productsWithUserFavourites
+        }
     }
 }
