4c9adfed9139b66ec1889c44f26f3f3ab8d5a7ab
Limiel104 <a.myjak1995@gmail.com>
2023-10-25 21:57:31 +0200

    Profile screen refactor

--- app/src/main/java/com/example/shopapp/presentation/profile/composable/ProfileContent.kt
+++ app/src/main/java/com/example/shopapp/presentation/profile/composable/ProfileContent.kt
@@ -1,15 +1,16 @@
 package com.example.shopapp.presentation.profile.composable
 
 import android.annotation.SuppressLint
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
+import androidx.compose.material3.Button
 import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -18,21 +19,13 @@ import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.shopapp.R
-import com.example.shopapp.presentation.common.composable.ErrorTextFieldItem
-import com.example.shopapp.presentation.common.composable.ShopButtonItem
-import com.example.shopapp.presentation.common.composable.ShopTextFieldItem
-import com.example.shopapp.util.Constants.PROFILE_CITY_ERROR
 import com.example.shopapp.util.Constants.PROFILE_CITY_TF
 import com.example.shopapp.util.Constants.PROFILE_COLUMN
 import com.example.shopapp.util.Constants.PROFILE_CONTENT
 import com.example.shopapp.util.Constants.PROFILE_CPI
-import com.example.shopapp.util.Constants.PROFILE_FIRSTNAME_ERROR
 import com.example.shopapp.util.Constants.PROFILE_FIRSTNAME_TF
-import com.example.shopapp.util.Constants.PROFILE_LASTNAME_ERROR
 import com.example.shopapp.util.Constants.PROFILE_LASTNAME_TF
-import com.example.shopapp.util.Constants.PROFILE_STREET_ERROR
 import com.example.shopapp.util.Constants.PROFILE_STREET_TF
-import com.example.shopapp.util.Constants.PROFILE_ZIP_CODE_ERROR
 import com.example.shopapp.util.Constants.PROFILE_ZIP_CODE_TF
 import com.example.shopapp.util.Constants.SAVE_BTN
 import com.example.shopapp.util.Constants.cityEmptyError
@@ -61,7 +54,7 @@ fun ProfileContent(
     isLoading: Boolean,
     onGoBack: () -> Unit,
     onSave: () -> Unit
-    ) {
+) {
     Scaffold(
         topBar = {
             ProfileTopBar(
@@ -75,131 +68,88 @@ fun ProfileContent(
             modifier = Modifier
                 .fillMaxSize()
                 .padding(paddingValues)
-                .padding(10.dp)
                 .testTag(PROFILE_COLUMN),
+            verticalArrangement = Arrangement.SpaceBetween,
+            horizontalAlignment = Alignment.CenterHorizontally
         ) {
-            Column(
-                modifier = Modifier
-                    .weight(1F)
-            ) {
-                ShopTextFieldItem(
-                    text = firstName,
-                    label = stringResource(id = R.string.first_name),
-                    placeholder = stringResource(id = R.string.first_name),
-                    testTag = PROFILE_FIRSTNAME_TF,
-                    isError = firstNameError != null,
-                    onValueChange = { onFirstNameChange(it) }
+            OutlinedTextField(
+                value = firstName,
+                onValueChange = { onFirstNameChange(it) },
+                label = { Text(text = stringResource(id = R.string.first_name)) },
+                placeholder = { Text(text = stringResource(id = R.string.first_name)
+                ) },
+                supportingText = {
+                    if(firstNameError != null) {
+                        Text(text = firstNameError)
+                    } },
+                isError = firstNameError != null,
+                singleLine = true,
+                modifier = Modifier.testTag(PROFILE_FIRSTNAME_TF)
+            )
+
+            OutlinedTextField(
+                value = lastName,
+                onValueChange = { onLastNameChange(it) },
+                label = { Text(text = stringResource(id = R.string.last_name)) },
+                placeholder = { Text(text = stringResource(id = R.string.last_name)) },
+                supportingText = {
+                    if(lastNameError != null) {
+                        Text(text = lastNameError)
+                    } },
+                isError = lastNameError != null,
+                singleLine = true,
+                modifier = Modifier.testTag(PROFILE_LASTNAME_TF)
+            )
+
+            OutlinedTextField(
+                value = street,
+                onValueChange = { onStreetChange(it) },
+                label = { Text(text = stringResource(id = R.string.street)) },
+                placeholder = { Text(text = stringResource(id = R.string.street)) },
+                supportingText = {
+                    if(streetError != null) {
+                        Text(text = streetError)
+                    } },
+                isError = streetError != null,
+                singleLine = true,
+                modifier = Modifier.testTag(PROFILE_STREET_TF)
+            )
+
+            OutlinedTextField(
+                value = city,
+                onValueChange = { onCityChange(it) },
+                label = { Text(text = stringResource(id = R.string.city)) },
+                placeholder = { Text(text = stringResource(id = R.string.city)) },
+                supportingText = {
+                    if(cityError != null) {
+                        Text(text = cityError)
+                    } },
+                isError = cityError != null,
+                singleLine = true,
+                modifier = Modifier.testTag(PROFILE_CITY_TF)
                 )
 
-                if(firstNameError != null) {
-                    ErrorTextFieldItem(
-                        errorMessage = firstNameError,
-                        testTag = PROFILE_FIRSTNAME_ERROR
-                    )
-                }
-            }
+            OutlinedTextField(
+                value = zipCode,
+                onValueChange = { onZipCodeChange(it) },
+                label = { Text(text = stringResource(id = R.string.zip_code)) },
+                placeholder = { Text(text = stringResource(id = R.string.zip_code)) },
+                supportingText = {
+                    if(zipCodeError != null) {
+                        Text(text = zipCodeError)
+                    } },
+                isError = zipCodeError != null,
+                singleLine = true,
+                modifier = Modifier.testTag(PROFILE_ZIP_CODE_TF)
+            )
 
-            Column(
+            Button(
                 modifier = Modifier
-                    .weight(1F)
+                    .padding(bottom = 10.dp)
+                    .testTag(SAVE_BTN),
+                onClick = { onSave() }
             ) {
-                ShopTextFieldItem(
-                    text = lastName,
-                    label = stringResource(id = R.string.last_name),
-                    placeholder = stringResource(id = R.string.last_name),
-                    testTag = PROFILE_LASTNAME_TF,
-                    isError = lastNameError != null,
-                    onValueChange = { onLastNameChange(it) }
-                )
-
-                if(lastNameError != null) {
-                    ErrorTextFieldItem(
-                        errorMessage = lastNameError,
-                        testTag = PROFILE_LASTNAME_ERROR
-                    )
-                }
-            }
-
-            Column(
-                modifier = Modifier
-                    .weight(1F)
-            ) {
-                ShopTextFieldItem(
-                    text = street,
-                    label = stringResource(id = R.string.street),
-                    placeholder = stringResource(id = R.string.street),
-                    testTag = PROFILE_STREET_TF,
-                    isError = streetError != null,
-                    onValueChange = { onStreetChange(it) }
-                )
-
-                if(streetError != null) {
-                    ErrorTextFieldItem(
-                        errorMessage = streetError,
-                        testTag = PROFILE_STREET_ERROR
-                    )
-                }
-            }
-
-            Column(
-                modifier = Modifier
-                    .weight(1F)
-            ) {
-                Row() {
-                    Column(
-                        modifier = Modifier.weight(1F)
-                    ) {
-                        ShopTextFieldItem(
-                            text = city,
-                            label = stringResource(id = R.string.city),
-                            placeholder = stringResource(id = R.string.city),
-                            testTag = PROFILE_CITY_TF,
-                            isError = cityError != null,
-                            onValueChange = { onCityChange(it) }
-                        )
-
-                        if(cityError != null) {
-                            ErrorTextFieldItem(
-                                errorMessage = cityError,
-                                testTag = PROFILE_CITY_ERROR
-                            )
-                        }
-                    }
-
-                    Spacer(modifier = Modifier.width(5.dp))
-
-                    Column(
-                        modifier = Modifier.weight(1F)
-                    ) {
-                        ShopTextFieldItem(
-                            text = zipCode,
-                            label = stringResource(id = R.string.zip_code),
-                            placeholder = stringResource(id = R.string.zip_code),
-                            testTag = PROFILE_ZIP_CODE_TF,
-                            isError = zipCodeError != null,
-                            onValueChange = { onZipCodeChange(it) }
-                        )
-
-                        if(zipCodeError != null) {
-                            ErrorTextFieldItem(
-                                errorMessage = zipCodeError,
-                                testTag = PROFILE_ZIP_CODE_ERROR
-                            )
-                        }
-                    }
-                }
-            }
-
-            Column(
-                modifier = Modifier
-                    .padding(top = 20.dp)
-                    .weight(2F)
-            ) {
-                ShopButtonItem(
-                    text = stringResource(id = R.string.save),
-                    testTag = SAVE_BTN,
-                    onClick = { onSave() }
-                )
+                Text(text =stringResource(id = R.string.save))
             }
         }
     }
